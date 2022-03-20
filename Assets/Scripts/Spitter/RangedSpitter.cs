0ae84c69685f1652d236153cebf7da3c64edc2f9
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2022-03-20 17:33:58 +0100

    Added spitter

---
+++ Assets/Scripts/Spitter/RangedSpitter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedSpitter : Mob
+{
+    bool pursuit;
+    bool retreat;
+    private void Update() {
+        if (!target) return;
+        if ((-transform.position + target.position).sqrMagnitude <= viewDistance * viewDistance) {
+            animator.SetTrigger("Spotted");
+            animator.SetBool("InPursuit", true);
+            pursuit = true;
+            navMesh.destination = target.position;
+
+        } else if (pursuit) {
+            pursuit = false;
+            animator.SetBool("InPursuit", false);
+            navMesh.destination = spawnPos;
+            retreat = true;
+        }
+        if ((-transform.position + target.position).sqrMagnitude <= attackDistance * attackDistance) {
+            animator.SetTrigger("Attack");
+        }
+        if (retreat && (-transform.position + spawnPos).sqrMagnitude <= distanceFromBase * distanceFromBase) {
+            animator.SetBool("NearBase", true);
+            navMesh.destination = transform.position;
+            retreat = false;
+        } else if ((-transform.position + spawnPos).sqrMagnitude > distanceFromBase * distanceFromBase) {
+            animator.SetBool("NearBase", false);
+        }
+
+    }
+
+
+    public void AttackBegin() {
+
+    }
+    public void AttackEnd() {
+
+    }
+}
---
+++ Assets/Scripts/Spitter/SpitterAudio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitterAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+    public void Shoot() {
+
+    }
+    public void PlayStep() {
+
+    }
+
+    public void Grunt() {
+
+    }
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
