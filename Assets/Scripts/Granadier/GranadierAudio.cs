7cedb3922e7fe4754fa71d423c3d254df13eef2f
BlueTonio <82876244+BlueTonio@users.noreply.github.com>
2022-03-23 19:44:35 +0100

    granadier start

---
+++ Assets/Scripts/Granadier/GranadierAudio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GranadierAudio : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    public void PlayStep()
+    {
+
+    }
+
+    public void Grunt()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Granadier/MeleeGranadier.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeGranadier : Mob
+{
+    [SerializeField] Transform ellen;
+    bool pursuit;
+    bool retreat;
+    [SerializeField] AttackArea attackArea;
+    override public void Update()
+    {
+        if ((-transform.position + ellen.position).sqrMagnitude <= viewDistance * viewDistance)
+        {
+            animator.SetBool("InPursuit", true);
+            pursuit = true;
+            targetPos = ellen.position;
+
+        }
+        else if (pursuit)
+        {
+            pursuit = false;
+            animator.SetBool("InPursuit", false);
+            targetPos = spawnPos;
+            retreat = true;
+        }
+        if ((-transform.position + ellen.position).sqrMagnitude <= attackDistance * attackDistance)
+        {
+            animator.SetTrigger("MeleeAttack");
+        }
+        if (retreat && (-transform.position + spawnPos).sqrMagnitude <= distanceFromBase * distanceFromBase)
+        {
+            animator.SetBool("InPursuit", true);
+            targetPos = transform.position;
+            retreat = false;
+        }
+        else if ((-transform.position + spawnPos).sqrMagnitude > distanceFromBase * distanceFromBase)
+        {
+            animator.SetBool("InPursuit", false);
+        }
+
+        if ((targetPos - transform.position).sqrMagnitude < 0.5f) return;
+        base.Update();
+    }
+
+    public void AttackBegin()
+    {
+        attackArea.AttackStart();
+    }
+    public void AttackEnd()
+    {
+        attackArea.AttackEnd();
+    }
+
+}
