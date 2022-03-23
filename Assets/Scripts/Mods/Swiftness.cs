41c43fc7a03b800baa48b12dd98b5f8d4d24b997
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2022-03-23 17:46:49 +0100

    particles fix

--- Assets/Scripts/Mods/Swiftness.cs
+++ Assets/Scripts/Mods/Swiftness.cs
@@ -6,13 +6,21 @@ using UnityEngine.AI;
 public class Swiftness : Mod
 {
     [SerializeField] float speedMult = 1;
+    [SerializeField] ParticleSystem particlesPrefab;
+    ParticleSystem particles;
+
     float oldSpeed;
     public override void Activate() {
         oldSpeed = attachedTo.GetComponent<BaseController>().speed;
         attachedTo.GetComponent<BaseController>().speed = oldSpeed * speedMult;
+        particles = GameObject.Instantiate(particlesPrefab, attachedTo.transform);
+        ParticleSystem.ShapeModule s = particles.shape;
+        s.skinnedMeshRenderer = attachedTo.GetComponentInChildren<SkinnedMeshRenderer>();
+
     }
     public override void Disable() {
         attachedTo.GetComponent<NavMeshAgent>().speed = oldSpeed;
         attachedTo.GetComponent<BaseController>().speed = oldSpeed;
+        Destroy(particles.gameObject);
     }
 }
