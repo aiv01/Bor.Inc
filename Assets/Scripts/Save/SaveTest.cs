fe71b725709ff45bd9b4a3dc2dc36a88522a96d1
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2022-04-03 13:31:20 +0200

    save system

---
+++ Assets/Scripts/Save/SaveTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveTest : MonoBehaviour
+{
+    public SaveObject so;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SaveManager.Save(so);
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            so = SaveManager.Load();
+        }
+    }
+}
