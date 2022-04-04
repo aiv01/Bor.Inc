e010d6dd729abc132713d280735bd9b96c180b3a
BlueTonio <82876244+BlueTonio@users.noreply.github.com>
2022-04-04 11:33:50 +0200

    final scene add

---
+++ Assets/Scripts/UI/ButtonFinalScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class ButtonFinalScene : MonoBehaviour
+{
+    private Player player;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetButtonDown("Select")) { NewGame();}
+        if (player.GetButtonDown("OpenVendor")) { ExitGame();}
+    }
+
+    public void NewGame()
+    {
+        ScriptableStaticClass.instance.Clear();
+        SaveManager.SaveStaticClassValues();
+        SceneManager.LoadScene("lobby");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+
+}
