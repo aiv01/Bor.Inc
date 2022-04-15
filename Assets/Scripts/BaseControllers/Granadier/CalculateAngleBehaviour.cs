de710031c6d34fa697dc83663011de0c4d0d187a
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2022-04-15 13:25:06 +0200

    Update CalculateAngleBehaviour.cs

--- Assets/Scripts/BaseControllers/Granadier/CalculateAngleBehaviour.cs
+++ Assets/Scripts/BaseControllers/Granadier/CalculateAngleBehaviour.cs
@@ -12,13 +12,11 @@ public class CalculateAngleBehaviour : StateMachineBehaviour
         Vector3 from = animator.transform.forward;
         Vector3 to = (mg.Ellen.position - animator.transform.position).normalized;
         float angle = Vector3.SignedAngle(from, to, Vector3.up);
-        int sign = angle > 0 ? 1 : -1;
         animator.SetFloat("Angle", angle * 0.0055556f);
         angle = Mathf.Abs(angle);
         Debug.Log(angle);
         if (angle > 150) {
             animator.SetTrigger("CloseAreaAttack");
-            //animator.SetBool("Turn", true);
             return;
         }else if(angle > mg.DetecAngle)
         {
@@ -40,28 +38,4 @@ public class CalculateAngleBehaviour : StateMachineBehaviour
         }
         animator.SetBool("Turn", false);
     }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
